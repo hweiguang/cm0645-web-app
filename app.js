e5a2d797d2e7e0b807939393fbfe6f5eefdea17f
Wei Guang <weiguang@icloud.com>
2017-01-18 23:59:24 +0000

    Update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(8080, function() {
-  console.log('Node app is running on port 8080');
+app.get('/', function(request, response) {
+  response.response('Hello world!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
